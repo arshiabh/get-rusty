ed9b230ee16c87e00312129ec2c103e92880e628
arshia bohlooli <arshiabohloli@gmail.com>
2025-08-20 19:54:27 +0330

    created loop and finished the game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,22 +3,29 @@ use std::{cmp::Ordering, io};
 use rand::Rng;
 
 fn main() {
-    let mut guess = String::new();
     let secret_num = rand::thread_rng().gen_range(1..=100);
 
-    println!("here is the guessing_game !!");
-    println!("secret number is {}", secret_num);
+    println!("guess the number !!");
 
-    println!("take your guess");
-    io::stdin().read_line(&mut guess).expect("catch error");
+    loop {
+        let mut guess = String::new();
+        println!("take your guess: ");
+        io::stdin().read_line(&mut guess).expect("catch error");
 
-    let guess: u32 = guess.trim().parse().expect("expected number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("your guessed number is {}", guess);
+        println!("your guess is: {}", guess);
 
-    match guess.cmp(&secret_num) {
-        Ordering::Less => println!("your number is smaller "),
-        Ordering::Equal => println!("your guess is correct"),
-        Ordering::Greater => println!("your number is greater"),
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("your number is smaller "),
+            Ordering::Greater => println!("your number is greater"),
+            Ordering::Equal => {
+                println!("your guess is correct");
+                break;
+            }
+        }
     }
 }
