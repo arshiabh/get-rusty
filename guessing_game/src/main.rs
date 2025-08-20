2be03dcf92b92264e8ed4c554449effda6d14fab
arshia bohlooli <arshiabohloli@gmail.com>
2025-08-20 18:02:58 +0330

    guessing game is created

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut x = String::new();
+    println!("here is the guessing_game lol!!");
+
+    io::stdin().read_line(&mut x).expect("catch error");
+
+    println!("your number is {} :))", x)
 }
